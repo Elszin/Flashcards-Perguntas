63f56c002b985488fad4ad764c22ea50e2bdc2b6
Eliabe Antonio <130597081+Elszin@users.noreply.github.com>
2024-09-30 16:00:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Oque significa a sigla ONU? e qual a sua utilidade?',
+    'A Organização das Nações Unidas, também conhecida pela sigla ONU, é uma organização internacional formada por países que se reuniram voluntariamente para trabalhar pela paz e o desenvolvimento mundiais.'
+)
+
+criaCartao(
+    'Programação',
+    'Para que utilizamos a tag "p" na programação?',
+    'A tag "p" em HTML é usada para definir um parágrafo.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual seria a tradução para footer?',
+    'A tradução de footer é rodapé'
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve um footer?',
+    'O elemento footer marca a área inferior, normalmente conhecida como rodapé, do conteúdo geral do documento ou do conteúdo de uma seção específica a qual ele está subordinado.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o maior fato historico do Brasil?',
+    'A chegada dos portugueses ou o descobrimento do Brasil foi um acontecimento que se passou em 22 de abril de 1500 e marcou o início da colonização brasileira.'
+)
+
+criaCartao(
+    'História',
+    'Oque é capitalismo?',
+    'O capitalismo é um sistema econômico baseado na propriedade privada dos meios de produção e na liberdade de mercado.'
+)
