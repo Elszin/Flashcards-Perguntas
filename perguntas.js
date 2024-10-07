09733da19adf4aae86d537d91e084819ce3a7e82
Eliabe Antonio <130597081+Elszin@users.noreply.github.com>
2024-10-07 15:25:40 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -6,36 +6,18 @@ criaCartao(
 
 criaCartao(
     'Geografia',
-    'Oque significa a sigla ONU? e qual a sua utilidade?',
-    'A Organização das Nações Unidas, também conhecida pela sigla ONU, é uma organização internacional formada por países que se reuniram voluntariamente para trabalhar pela paz e o desenvolvimento mundiais.'
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
     'Programação',
-    'Para que utilizamos a tag "p" na programação?',
-    'A tag "p" em HTML é usada para definir um parágrafo.'
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Programação',
-    'Qual seria a tradução para footer?',
-    'A tradução de footer é rodapé'
-)
-
-criaCartao(
-    'Programação',
-    'Para que serve um footer?',
-    'O elemento footer marca a área inferior, normalmente conhecida como rodapé, do conteúdo geral do documento ou do conteúdo de uma seção específica a qual ele está subordinado.'
-)
-
-criaCartao(
-    'História',
-    'Qual foi o maior fato historico do Brasil?',
-    'A chegada dos portugueses ou o descobrimento do Brasil foi um acontecimento que se passou em 22 de abril de 1500 e marcou o início da colonização brasileira.'
-)
-
-criaCartao(
-    'História',
-    'Oque é capitalismo?',
-    'O capitalismo é um sistema econômico baseado na propriedade privada dos meios de produção e na liberdade de mercado.'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
